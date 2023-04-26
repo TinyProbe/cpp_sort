a4bc8d550232e8e81ebb64c384db743ee966b5d6
TinyProbe <137adv167@gmail.com>
2023-04-27 04:53:07 +0900

    Update count_sort.cpp

--- count_sort.cpp
+++ count_sort.cpp
@@ -8,9 +8,9 @@ void count_sort(vector<int>& v) {
 	int neg_max = 0, pos_max = 0;
 	for (auto& e : v)
 		if (e < 0)
-			neg.push_back((neg_max < -e ? neg_max = -e : -e));
+			neg.push_back(neg_max < -e ? neg_max = -e : -e);
 		else
-			pos.push_back((pos_max < e ? pos_max = e : e));
+			pos.push_back(pos_max < e ? pos_max = e : e);
 	abs_count_sort(neg, neg_max);
 	abs_count_sort(pos, pos_max);
 	for (int i = 0; i < (int) neg.size(); ++i)
