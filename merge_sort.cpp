8e07518fb27ebc83813528c770524423bb444dd4
TinyProbe <137adv167@gmail.com>
2023-04-28 18:13:28 +0900

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -6,14 +6,14 @@ void merge_sort(vector<int>& v) {
 	vector<vector<int>> vv(2);
 	vv[0] = std::move(v);
 	vv[1].resize(vv[0].size());
-	while ((1 << (seq - 1)) <= n) {
+	while ((1 << (seq - 1)) < n) {
 		int cur = 0, unit = 1 << seq, half = unit >> 1;
 		while (cur < n) {
 			int i = cur, l = cur, r = cur + half;
-			int ll = r, rr = min(cur + unit, n);
+			int ll = min(r, n), rr = min(cur + unit, n);
 			while (l < ll || r < rr)
 				if (l < ll && r < rr)
-					vv[seq & 1][i++] = vv[!(seq & 1)][l] < vv[!(seq & 1)][r] ? vv[!(seq & 1)][l++] : vv[!(seq & 1)][r++];
+					vv[seq & 1][i++] = (vv[!(seq & 1)][l] < vv[!(seq & 1)][r] ? vv[!(seq & 1)][l++] : vv[!(seq & 1)][r++]);
 				else if (l < ll)
 					vv[seq & 1][i++] = vv[!(seq & 1)][l++];
 				else
