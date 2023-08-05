b313d214e62e0f016e56b7441242e0b2e6846efe
TinyProbe <137adv167@gmail.com>
2023-08-05 17:54:15 +0900

    update

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -2,25 +2,27 @@
 using namespace std;
 
 void merge_sort(vector<long> &v) {
-	int seq = 1, n = v.size();
-	vector<vector<int>> vv(2);
+	size_t seq=1, n=v.size();
+	vector<vector<long>> vv(2);
 	vv[0] = std::move(v);
 	vv[1].resize(vv[0].size());
-	while ((1 << (seq - 1)) < n) {
-		int cur = 0, unit = 1 << seq, half = unit >> 1;
+	while ((1<<(seq-1)) < n) {
+		long cur=0, unit=1<<seq, half=unit>>1;
 		while (cur < n) {
-			int i = cur, l = cur, r = cur + half;
-			int ll = min(r, n), rr = min(cur + unit, n);
-			while (l < ll || r < rr)
-				if (l < ll && r < rr)
-					vv[seq & 1][i++] = (vv[!(seq & 1)][l] < vv[!(seq & 1)][r] ? vv[!(seq & 1)][l++] : vv[!(seq & 1)][r++]);
-				else if (l < ll)
-					vv[seq & 1][i++] = vv[!(seq & 1)][l++];
-				else
-					vv[seq & 1][i++] = vv[!(seq & 1)][r++];
+			long i=cur, l=cur, r=cur+half;
+			long ll=min(r, n), rr=min(cur+unit, n);
+			while (l<ll || r<rr) {
+				if (l<ll && r<rr) {
+					vv[seq&1][i++] = (vv[!(seq&1)][l] < vv[!(seq&1)][r] ? vv[!(seq&1)][l++] : vv[!(seq&1)][r++]);
+				} else if (l < ll) {
+					vv[seq&1][i++] = vv[!(seq&1)][l++];
+				} else {
+					vv[seq&1][i++] = vv[!(seq&1)][r++];
+				}
+			}
 			cur += unit;
 		}
-		++seq;
+		seq += 1;
 	}
-	v = std::move(vv[!(seq & 1)]);
+	v = std::move(vv[!(seq&1)]);
 }
--- radix_sort.cpp
+++ radix_sort.cpp
@@ -1,41 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-static void abs_radix_sort(vector<queue<int>>& qvec, int max_value);
+static void abs_radix_sort(vector<queue<long>> &qvec, size_t max_value);
 
-void radix_sort(vector<int>& v) {
-	vector<queue<int>> neg(2), pos(2);
-	int neg_max = 0, pos_max = 0;
-	for (auto& e : v)
-		if (e < 0)
-			neg.back().push(neg_max < -e ? neg_max = -e : -e);
-		else
-			pos.back().push(pos_max < e ? pos_max = e : e);
+void radix_sort(vector<long> &v) {
+	vector<queue<long>> neg(2), pos(2);
+	size_t neg_max=0, pos_max=0;
+	for (auto &e: v) {
+		if (e < 0) { neg.back().push(neg_max<-e ? neg_max=-e : -e); }
+		else { pos.back().push(pos_max<e ? pos_max=e : e); }
+	}
 	abs_radix_sort(neg, neg_max);
 	abs_radix_sort(pos, pos_max);
-	for (int i = neg.front().size(); i--; )
+	for (size_t i=neg.front().size(); i--; ) {
 		v[i] = -neg.front().front(), neg.front().pop();
-	for (int i = v.size() - pos.front().size(); i < (int) v.size(); ++i)
+	}
+	for (size_t i=v.size()-pos.front().size(); i<v.size(); ++i) {
 		v[i] = pos.front().front(), pos.front().pop();
+	}
 }
 
-static void abs_radix_sort(vector<queue<int>>& qvec, int max_value) {
-	int base = 2;
-	while (base * base < (int) qvec.back().size())
-		base <<= 1;
+static void abs_radix_sort(vector<queue<long>> &qvec, size_t max_value) {
+	size_t base=2, div=1;
+	while (base*base < qvec.back().size()) { base<<=1; }
 	qvec.resize(base);
-	vector<int> siz(base);
-	int div = 1;
+	vector<size_t> siz(base);
 	while (true) {
-		for (int i = 0; i < base; ++i)
+		for (size_t i=0; i<base; ++i) {
 			siz[i] = qvec[i].size();
-		for (int i = 0; i < base; ++i)
+		}
+		for (size_t i=0; i<base; ++i) {
 			while (siz[i]--) {
-				int val = qvec[i].front(); qvec[i].pop();
-				qvec[val / div % base].push(val);
+				size_t val = qvec[i].front();
+				qvec[i].pop();
+				qvec[val/div%base].push(val);
 			}
-		if (div > max_value)
-			break;
-		div *= base;
+		}
+		if (div>max_value) { break; }
+		div*=base;
 	}
 }
